d64bb07ac37a17ec10fb3de30cb4d07b97a40fb5
Torrey Meade <themeadeconcept@hotmail.com>
2020-11-30 18:31:59 -0500

    Added tinder cards and some swiping functionality, and styles using React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
+// import TinderCard from "react-tinder-card";
 import "./App.css";
 import Header from "./Header";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
     // BEM class naming convention
     <div className="app">
       <Header />
-      {/* TinderCards */}
+      <TinderCards />
       {/* SwipeButtons */}
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Header.css";
+import FaceIcon from "@material-ui/icons/Face";
+import IconButton from "@material-ui/core/IconButton";
+import ForumIcon from "@material-ui/icons/Forum";
 
 function Header() {
   return (
     <div className="header">
-      <h2>I am a Header</h2>
+      <IconButton>
+        <FaceIcon fontSize="large" className="header__icon" />
+      </IconButton>
+
+      <img
+        className="header__logo"
+        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+        alt=""
+      />
+
+      <IconButton>
+        <ForumIcon fontSize="large" className="header__icon" />
+      </IconButton>
     </div>
   );
 }
